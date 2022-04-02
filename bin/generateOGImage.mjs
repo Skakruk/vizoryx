de7af6914fe7898a291c0fd6694c666f31f89897
Skakruk <skakruk15@gmail.com>
2022-04-02 16:52:58 +0300

    Generate Open Graph tags

---
+++ bin/generateOGImage.mjs
@@ -0,0 +1,113 @@
+import canvasPkg from 'canvas';
+import path from 'path';
+import { readFile, writeFile } from 'fs/promises';
+
+const excludeFromTotals = ['man-portable air defence systems', 'anti-tank guided missiles'];
+
+const statuses = [
+  'destroyed',
+  'damaged',
+  'abandoned',
+  'captured'
+];
+
+const run = async () => {
+  const stats = JSON.parse(
+    await readFile(
+      new URL('../src/statistics.json', import.meta.url)
+    )
+  );
+
+  const { Russia: rusStats, Ukraine: ukrStats } = stats.reduce((acc, entry) => {
+    if (!acc[entry.country]) {
+      acc[entry.country] = {
+        total: 0,
+        captured: 0,
+        destroyed: 0,
+        damaged: 0,
+        abandoned: 0,
+      };
+    }
+    if (!excludeFromTotals.includes(entry.type.toLowerCase())) {
+      acc[entry.country].total += entry.statuses.total;
+      acc[entry.country].captured += entry.statuses.captured ?? 0;
+      acc[entry.country].destroyed += entry.statuses.destroyed ?? 0;
+      acc[entry.country].damaged += entry.statuses.damaged ?? 0;
+      acc[entry.country].abandoned += entry.statuses.abandoned ?? 0;
+    }
+    return acc;
+  }, {});
+
+  const { createCanvas, registerFont } = canvasPkg;
+
+  const width = 1200;
+  const height = 630;
+
+  registerFont(path.resolve('./bin/assets/BebasNeue-Regular.ttf'), {
+    family: 'BebasNeue',
+  });
+
+  const canvas = createCanvas(width, height);
+  const context = canvas.getContext('2d');
+
+  context.fillStyle = '#1f1f1f';
+  context.fillRect(0, 0, width, height);
+
+  context.font = `regular 70pt 'BebasNeue'`;
+  context.textAlign = 'center';
+  context.textBaseline = 'top';
+  context.fillStyle = '#fff';
+
+  context.fillText('Visual confirmed losses', 600, 40);
+
+  context.font = `regular 55pt 'BebasNeue'`;
+  context.textAlign = 'left';
+  context.textBaseline = 'top';
+  context.fillText('Russia', 50, 180);
+
+  context.textAlign = 'right';
+  context.fillText('Ukraine', 1150, 180);
+
+  const rusTextWidth = context.measureText('Russia').width;
+  const ukrTextWidth = context.measureText('Ukraine').width;
+
+  context.fillStyle = '#f00';
+  context.font = `regular 40pt 'BebasNeue'`;
+  context.textAlign = 'left';
+  context.fillText(rusStats.total, rusTextWidth + 80, 192);
+
+  statuses.map((status, idx) => {
+    context.fillText(rusStats[status], 50, 300 + idx * 70);
+  });
+
+  context.textAlign = 'right';
+  context.fillText(ukrStats.total, width - ukrTextWidth - 80, 192);
+
+  statuses.map((status, idx) => {
+    context.fillText(ukrStats[status], 1150, 300 + idx * 70);
+  });
+
+  context.fillStyle = '#fff';
+  context.textAlign = 'center';
+
+  statuses.map((status, idx) => {
+    context.fillText(status, 600, 300 + idx * 70);
+  });
+
+
+  context.fillStyle = '#cbcbcb';
+  context.fillRect(0, height - 15, width, height);
+
+  context.fillStyle = '#ec0202';
+  if (rusStats.total > ukrStats.total) {
+    context.fillRect(0, height - 15, width * (rusStats.total / (rusStats.total + ukrStats.total)), height);
+  } else {
+    context.fillRect(width * (ukrStats.total / (rusStats.total + ukrStats.total)), height - 15, width, height);
+  }
+
+  const buffer = canvas.toBuffer('image/png');
+  await writeFile('./dist/assets/card-image.png', buffer);
+};
+
+
+run();
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,87 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import htmlConfig from 'vite-plugin-html-config';
+import pkg from './package.json';
+
+const CARD_IMAGE_PATH = '/assets/card-image.png';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => {
+  const DOMAIN = mode === 'production' ? pkg.homepage : 'http://localhost:3000';
+  const getURL = (path = '') => new URL(path, DOMAIN).toString();
+
+  const htmlPluginOpt = {
+    headScripts: mode === 'production' ? [
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-EN3MB859BW',
+      },
+      `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-EN3MB859BW');
+      `,
+    ] : [],
+
+    metas: [
+      {
+        name: 'og:url',
+        content: getURL(),
+      },
+      {
+        name: 'og:type',
+        content: 'website',
+      },
+      {
+        name: 'og:title',
+        content: pkg.displayName,
+      },
+      {
+        name: 'og:description',
+        content: pkg.description,
+      },
+      {
+        name: 'og:image',
+        content: getURL(CARD_IMAGE_PATH),
+      },
+      {
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        name: 'twitter:domain',
+        content: new URL(DOMAIN).hostname,
+      },
+      {
+        name: 'twitter:url',
+        content: getURL(),
+      },
+      {
+        name: 'twitter:title',
+        content: pkg.displayName,
+      },
+      {
+        name: 'twitter:description',
+        content: pkg.description,
+      },
+      {
+        name: 'twitter:image',
+        content: getURL(CARD_IMAGE_PATH),
+      },
+      {
+        name: 'robots',
+        content:
+          mode === 'production'
+            ? 'index, follow'
+            : 'noindex, nofollow',
+      },
+    ],
+  };
+
+  return {
+    build: {
+      emptyOutDir: true,
+    },
+    plugins: [htmlConfig(htmlPluginOpt), react()]
+  }
+});
