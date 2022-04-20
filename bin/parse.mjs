9b2300691e03b2cb3150c257e1b4500309cdd9b8
Skakruk <skakruk15@gmail.com>
2022-04-20 20:54:27 +0300

    Fix parsing

--- bin/parse.mjs
+++ bin/parse.mjs
@@ -10,106 +10,92 @@ const replaceTypeName = {
   'Radars': 'Radars And Communications Equipment'
 };
 
-const run = async () => {
-  const { body: html } = await got.get('https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html');
-  // const html = await fs.readFile('./tmp/index.html', 'utf8');
-
-  const dom = parse(html);
-
-  const ukraineEl = dom.querySelectorAll('h3 span[style*="color: red;"]')
-    .find(el => el.textContent.startsWith('Ukraine'))
-    .parentNode;
-
-  const contentNode = ukraineEl.parentNode;
-  const ukraineElIndex = contentNode.childNodes.findIndex(el => el === ukraineEl);
-
-  const mechanics = dom.querySelectorAll('h3')
-    .filter(el => el && typeNameRegExp.test(el.textContent));
+(async () => {
+  const pages = {
+    'Ukraine': 'https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-ukrainian.html',
+    'Russia': 'https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html',
+  };
 
   const output = [];
 
-  for await (const el of mechanics) {
-    let { type } = typeNameRegExp.exec(el.textContent)?.groups;
+  for await (const country of Object.keys(pages)) {
+    const { body: html } = await got.get(pages[country]);
+    const dom = parse(html);
 
-    const statuses = ('total: ' + el.textContent)
-      .replace(type, '')
-      .replace('(', '')
-      .replace(')', '')
-      .replace('of which ', '')
-      .trim()
-      .split(',')
-      .map(t => t.trim())
-      .reduce((acc, t) => {
-        const [statusType, count] = t.split(': ');
-        acc[statusType] = Number(count?.trim());
-        return acc;
-      }, {});
+    const mechanics = dom.querySelectorAll('h3')
+      .filter(el => el && typeNameRegExp.test(el.textContent));
 
-    const elIndex = Math.max(
-      contentNode.childNodes.findIndex(chEl => chEl === el),
-      contentNode.childNodes.findIndex(chEl => chEl === el.parentNode)
-    );
+    for await (const el of mechanics) {
+        let { type } = typeNameRegExp.exec(el.textContent)?.groups;
 
-    let country = 'Russia';
-    if (ukraineElIndex < elIndex) {
-      country = 'Ukraine';
-    }
+        const statuses = ('total: ' + el.textContent)
+          .replace(type, '')
+          .replace('(', '')
+          .replace(')', '')
+          .replace('of which ', '')
+          .trim()
+          .split(',')
+          .map(t => t.trim())
+          .reduce((acc, t) => {
+            const [statusType, count] = t.split(': ');
+            acc[statusType] = Number(count?.trim());
+            return acc;
+          }, {});
 
-    const listOfMechanics = el.querySelectorAll('+ ul li');
+        const listOfMechanics = el.querySelectorAll('+ ul li');
 
-    const items = [];
+        const items = [];
 
-    for await (const lEl of listOfMechanics) {
-      const liText = lEl.textContent.trim().replace(/\u00a0/g, ' '); // nbsp
-      const { total, name } = machineryName.exec(liText)?.groups ?? {};
-      if (!name) continue;
+        for await (const lEl of listOfMechanics) {
+          const liText = lEl.textContent.trim().replace(/\u00a0/g, ' '); // nbsp
+          const { total, name } = machineryName.exec(liText)?.groups ?? {};
+          if (!name) continue;
 
-      const links = lEl.querySelectorAll('a')
-        .map(linkEl => ({
-          url: linkEl.attrs.href,
-          title: linkEl.textContent.replace('(', '').replace(')', ''),
-        }));
+          const links = lEl.querySelectorAll('a')
+            .map(linkEl => ({
+              url: linkEl.attrs.href,
+              title: linkEl.textContent.replace('(', '').replace(')', ''),
+            }));
 
-      const linksToCorrect = links
-        .map(link => {
-          if (link.url.toLowerCase().startsWith('https://postimg.cc/')) {
-            console.log('getting correct image link for', link.url);
-            return new Promise(async (resolve) => {
-              const { body } = await got.get(link.url);
-              const postimgDom = parse(body);
-              resolve({
-                title: link.title,
-                url: postimgDom.querySelector('#code_direct').attrs.value
-              });
+          const linksToCorrect = links
+            .map(link => {
+              if (link.url.toLowerCase().startsWith('https://postimg.cc/')) {
+                console.log('getting correct image link for', link.url);
+                return new Promise(async (resolve) => {
+                  const { body } = await got.get(link.url);
+                  const postimgDom = parse(body);
+                  resolve({
+                    title: link.title,
+                    url: postimgDom.querySelector('#code_direct').attrs.value
+                  });
+                });
+              }
+
+              return link;
             });
-          }
 
-          return link;
+          const itemLinks = await Promise.all(linksToCorrect);
+
+          items.push({
+            total: Number(total),
+            name,
+            links: itemLinks,
+          });
+        }
+
+        if (replaceTypeName[type]) {
+          type = replaceTypeName[type];
+        }
+
+        output.push({
+          country,
+          type,
+          statuses,
+          items,
         });
-
-      const itemLinks = await Promise.all(linksToCorrect);
-
-      items.push({
-        total: Number(total),
-        name,
-        links: itemLinks,
-      });
     }
-
-    if (replaceTypeName[type]) {
-      type = replaceTypeName[type];
-    }
-
-    output.push({
-      country,
-      type,
-      statuses,
-      items,
-    });
   }
 
   await fs.writeFile('./src/data/detailed.json', JSON.stringify(output, null, 2))
-}
-
-run();
+})();
 
